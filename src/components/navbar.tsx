21fc76bb165b16b865db9b77dac5a65e529dae36
Michael McCartney <mmccartneycs@gmail.com>
2023-07-17 13:02:09 -0400

    testing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,20 +3,28 @@ import Math from "./math";
 import Trivia from "./trivia";
 import Date from "./date";
 
+
 export default function Navbar() {
 
     return (
-        <>
-            <Link to={"/math"}>Math</Link>
-            <Link to={"/trivia"}>Trivia</Link>
-            <Link to={"/date"}>Date</Link>
+        
+        <div className="navbar-container">
+        <Link to={"/math"} className="nav-link">
+          Math
+        </Link>
+        <Link to={"/trivia"} className="nav-link">
+          Trivia
+        </Link>
+        <Link to={"/date"} className="nav-link">
+          Date
+        </Link>
 
             {/* <Routes>
                 <Route path="/math" element={ <Math/> } />
                 <Route path="/trivia" element={ <Trivia/> } />
                 <Route path="/date" element={ <Date/> } />
             </Routes> */}
-        </>
+        </div>
+    
     )
-
 }
--- src/components/trivia.tsx
+++ src/components/trivia.tsx
@@ -1,9 +1,30 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './TriviaFacts.css';
 
+const Trivia = () => {
+  const [number, setNumber] = useState('');
+  const [fact, setTrivia] = useState('');
 
-export default function Trivia() {
-    return (
-        <>
-            <p>Trivia Works!</p>
-        </>
-    )
-}
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setNumber(event.target.value);
+  };
+
+  const getTrivia = () => {
+    axios.get(`http://numbersapi.com/${number}`)
+      .then((response) => {
+        setTrivia(response.data);
+      });
+  };
+
+  return (
+    <div className="triviaCenter">
+        <>Enter A Number</>
+      <input type="text" value={number} onChange={handleChange} />
+      <button onClick={getTrivia}>Get Fact</button>
+      <p>{fact}</p>
+    </div>
+  );
+};
+
+export default Trivia;
