dd74dc45a29c33e67d25c7821b68bcadbc607589
Daiki Anjiki <daikianjiki@gmail.com>
2023-07-18 08:08:28 -0700

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Trivia from './components/trivia';
 import Math from './components/math';
-import Date from './components/date';
+import Date from './components/date/date';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <div className='navbar'>
-        <Navbar />
+          <Navbar />
         </div>
         <Routes>
-                <Route path="/math" element={ <Math/> } />
-                <Route path="/trivia" element={ <Trivia/> } />
-                <Route path="/date" element={ <Date/> } />
+          <Route path="/math" element={ <Math/> } />
+          <Route path="/trivia" element={ <Trivia/> } />
+          <Route path="/date" element={ <Date/> } />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,30 +1,12 @@
 import { Link, Route, Routes } from "react-router-dom";
-import Math from "./math";
-import Trivia from "./trivia";
-import Date from "./date";
-
 
 export default function Navbar() {
 
     return (
-        
         <div className="navbar-container">
-        <Link to={"/math"} className="nav-link">
-          Math
-        </Link>
-        <Link to={"/trivia"} className="nav-link">
-          Trivia
-        </Link>
-        <Link to={"/date"} className="nav-link">
-          Date
-        </Link>
-
-            {/* <Routes>
-                <Route path="/math" element={ <Math/> } />
-                <Route path="/trivia" element={ <Trivia/> } />
-                <Route path="/date" element={ <Date/> } />
-            </Routes> */}
+          <Link to={"/math"} className="nav-link">Math</Link>
+          <Link to={"/trivia"} className="nav-link">Trivia</Link>
+          <Link to={"/date"} className="nav-link">Date</Link>
         </div>
-    
     )
 }
